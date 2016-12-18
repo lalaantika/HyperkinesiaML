52177aa3edca58952ed4627e27250bd52cdaac7f
lalaantika <sharmal0707@gmail.com>
2016-12-18 18:28:51 +0530

    Delete myfile.py

--- MLHyperkinesaproject/myfile.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from subprocess import call
-
-pyprog = 'tem2.py'
-def callpy(): call(['python', '-i', pyprog] )
-
-root = Tk()
-Button(root, text='Run', command=callpy).pack()
-root.mainloop()
